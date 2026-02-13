e3ddafde323a10252963dda0e95d7ea4a98dc075
Jamie Yang <jy4474@nyu.edu>
2026-02-12 22:28:40 -0500

    Initial commit

---
+++ about/data.js
@@ -0,0 +1,45 @@
+const cardInfo = [
+
+    {
+        bgColor: '255,188,75',
+        imgUrl: 'img/goobers.png',
+        tips: 'I LOVE',
+        contnetText: 'My Cat Goobers'
+    },
+    {
+        bgColor: '255,208,213',
+        imgUrl: 'img/books.png',
+        tips: 'I AM CURRENTLY',
+        contnetText: 'Reading'
+    },
+    {
+        bgColor: '255,188,75',
+        imgUrl: 'img/cooking.png',
+        tips: 'I LOVE',
+        contnetText: 'Cooking and Baking'
+    },
+    {
+        bgColor: '164,201,216',
+        imgUrl: 'img/snowboard.jpeg',
+        tips: 'I LOVE',
+        contnetText: 'Snowboarding'
+    },
+    {
+        bgColor: '255,208,213',
+        imgUrl: 'img/music.png',
+        tips: 'I LOVE',
+        contnetText: 'Listening to Pop and Hip-Hop'
+    },
+    {
+        bgColor: '255,208,213',
+        imgUrl: 'img/food.png',
+        tips: 'I LOVE',
+        contnetText: 'Trying New Foods'
+    },
+    {
+        bgColor: '255,208,213',
+        imgUrl: 'img/3.JPG',
+        tips: 'WHO AM I',
+        contnetText: 'I am Jamie Yang'
+    },
+]
---
+++ contact/photo.js
@@ -0,0 +1,117 @@
+const photosInfo = [
+    {
+        cephalogram_url: './asset/img/Sagaya/1.jpg',
+        name: 'The Allure of Isolation',
+        introduction: 'Oct 2023',
+        size: 'width-24',
+        children: [
+            {
+                children_url: './asset/img/Sagaya/2.jpg',
+                children_introduction: `The landscapes seem frozen in time—an untouched wilderness where the road stretches endlessly into an unforgiving horizon. These photos invite you to a place that feels equal parts mysterious and serene, where nature’s silence speaks louder than any noise.  `,
+            },
+            {
+                children_url: './asset/img/Sagaya/3.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Sagaya/4.jpg',
+                children_introduction: ` Journeys are more than destinations—they are moments of connection with the earth, with our thoughts, and with the vast, endless possibilities that a lone road promises.`,
+            },
+            {
+                children_url: './asset/img/Sagaya/5.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Sagaya/6.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Sagaya/7.jpg',
+                children_introduction: `Sometimes, stillness is the most profound adventure of all.   `,
+            },
+        ]
+    },
+    {
+        cephalogram_url: './asset/img/An_Uncommon_Place_III/07440957155935.59cacacbd605e.jpg',
+        name: 'Untitlied',
+        introduction: '2024',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './asset/img/Sagaya/2.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/An_Uncommon_Place_III/55c9fc57155935.59cb068c0cdad.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/An_Uncommon_Place_III/8e704157155935.5c0eb753ae32d.jpg',
+                children_introduction: `a placeholder for blog content`,
+            },
+            {
+                children_url: './asset/img/An_Uncommon_Place_III/91b33c57155935.59cacacbd6a17.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/An_Uncommon_Place_III/a5beaf57155935.5c0eb7249caf9.jpg',
+                children_introduction: `a placeholder for blog content`,
+            },
+        ]
+    },
+    {
+        cephalogram_url: './asset/img/Between_Sea_ Sky/0c28e4151624419.630f528f3eded.jpg',
+        name: 'Untitled',
+        introduction: '2024',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './asset/img/Between_Sea_ Sky/af3dd8151624419.630f528f403bb.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Between_Sea_ Sky/b8fd0d151624419.630f528f3e60f.jpg',
+                children_introduction: `a placeholder for blog content`,
+            },
+            {
+                children_url: './asset/img/Between_Sea_ Sky/daf96a151624419.630f528f3d86f.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Between_Sea_ Sky/edff6b151624419.630f528f3df83.jpg',
+                children_introduction: `a placeholder for blog content`,
+            },
+        ]
+    },
+ 
+
+    {
+        cephalogram_url: './asset/img/Hong_Kong_Cinematic/083f6494251603.5e7a34fa62cfb.jpg',
+        name: 'HongKong',
+        introduction: '2023',
+        size: 'width-11',
+        children: [
+            {
+                children_url: './asset/img/Hong_Kong_Cinematic/0bfc9194251603.5e7a34fa5ff84.jpg',
+                children_introduction: `a placeholder for blog content`,
+            },
+            {
+                children_url: './asset/img/Hong_Kong_Cinematic/48f54994251603.5e7a5e86b0fac.jpg',
+                children_introduction: `a placeholder for blog content`,
+            },
+            {
+                children_url: './asset/img/Hong_Kong_Cinematic/cd5f5e94251603.5e7a629144bed.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Hong_Kong_Cinematic/dc31cc94251603.5e7a6f43a6bbc.jpg',
+                children_introduction: '',
+            },
+            {
+                children_url: './asset/img/Hong_Kong_Cinematic/e6903b94251603.5e7a34fa6e6fd.jpg',
+                children_introduction: '',
+            },
+        ]
+    },
+
+]
