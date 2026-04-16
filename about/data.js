cba9b3708f384faaae7e9b7cdbe5a4d27e9d41cd
Jamie Yang <jy4474@nyu.edu>
2026-04-16 14:12:58 -0400

    Initial commit

--- about/data.js
+++ about/data.js
@@ -10,13 +10,13 @@ const cardInfo = [
         bgColor: '255,208,213',
         imgUrl: 'img/books.png',
         tips: 'I AM CURRENTLY READING',
-        contentText: ''
+        contentText: 'User Friendly <br> Hooked <br> Continuous Discovery Habits <br> The Design of Everyday Things <br> Atomic Habits'
     },
     {
         bgColor: '255,208,213',
         imgUrl: 'img/books.png',
         tips: 'DESIGNING WITH PURPOSE',
-        contentText: 'I don’t just focus on how things look—I care about how they work, how they feel, <br>and how they impact real users. I aim to contribute thoughtful, user-centered <br>solutions while being a collaborative and proactive team member who helps <br> create meaningful and effective products.'
+        contentText: 'I don’t just focus on how things look—I care about how they work, how they feel, and how they impact real users. I aim to contribute thoughtful, user-centered solutions while being a collaborative and proactive team member who helps create meaningful and effective products.'
     },
     {
         bgColor: '255,208,213',
@@ -24,10 +24,9 @@ const cardInfo = [
         tips: 'WHO AM I',
         contentText: [
             "I am Jamie Yang, a Product designer based in New York." +
-            "<br> I’m a product designer focused on creating intuitive, human-centered digital <br>experiences feel thoughtful and easy to use." +
-            "I’m currently studying interactive <br> media at NYU, where I’ve developed a strong interest in UI/UX, user research, <br> and prototyping."+
-            " My work is driven by a curiosity about how people interact <br> with digital products, what feels seamless, what feels frustrating, and how design <br>can make everyday experiences more accessible and engaging." +
-            " With a background <br>in management, I approach design with both empathy and strategy in mind, <br>considering both the user experience and how products function within teams <br> and business goals."
+            "<br> I’m a product designer focused on creating intuitive, human-centered digital experiences feel thoughtful and easy to use." +
+            " I’m currently studying interactive <br> media at NYU, where I’ve developed a strong interest in UI/UX, user research, and prototyping." +
+            "<br>With a background in management, I approach design with both empathy and strategy in mind, considering both the user experience and how products function within teams and business goals."
         ]
     },
 ]
