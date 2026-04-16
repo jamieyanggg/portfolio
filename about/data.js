55f827f91f8916d845a79eb2862b6e4bbd7558cf
Jamie Yang <jy4474@nyu.edu>
2026-04-16 14:54:16 -0400

    Initial commit

--- about/data.js
+++ about/data.js
@@ -22,11 +22,10 @@ const cardInfo = [
         bgColor: '255,208,213',
         imgUrl: 'img/3.JPG',
         tips: 'WHO AM I',
-        contentText: [
+        contentText:
             "I am Jamie Yang, a Product designer based in New York." +
             "<br> I’m a product designer focused on creating intuitive, human-centered digital experiences feel thoughtful and easy to use." +
             " I’m currently studying interactive <br> media at NYU, where I’ve developed a strong interest in UI/UX, user research, and prototyping." +
             "<br>With a background in management, I approach design with both empathy and strategy in mind, considering both the user experience and how products function within teams and business goals."
-        ]
     },
 ]
